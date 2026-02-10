38ff54eddadf02cce9ca853de3972ce336b0c49d
Sahil Saeid <sahilsaeidkhan610@gmail.com>
2026-02-10 08:33:18 +0530

    numbers

---
+++ Codebasics - tutorials/numbers.py
@@ -0,0 +1,46 @@
+## find area length = 92 m and width = 48.8 m
+
+ #Area of rectangle =  l * b )
+
+foo =  92 * 48.8
+#print("The area of rectangle is " , foo)
+
+
+
+
+
+
+
+
+
+#You bought 9 packets of potato chips from a store. Each packet costs 1.49 dollar and you gave shopkeeper 20 dollar
+#Find out using python, how many dollars is the shopkeeper going to give you back?
+
+total_price = 9 * 1.49
+gave = 20
+will_received_back = gave - total_price
+#print(will_received_back)
+
+
+
+
+
+
+
+
+#2 length = 5.5m , cost of 1 square meter = 500 , total cost = ?
+
+total_area = 5.5**5.5
+total_cost = total_area * 500 
+#print(total_cost)
+
+
+
+
+
+# binary representation of 17
+
+n = 17
+print(format(n,"b"))
+
+
