4c74f039bf78836080500b95ecea0ddc1930a3a0
Sahil Saeid <sahilsaeidkhan610@gmail.com>
2026-02-13 11:39:21 +0530

    m

--- Codebasics - tutorials/dummy.py
+++ Codebasics - tutorials/dummy.py
@@ -1,16 +1,4 @@
-with open("codebasics - tutorials/stocks.csv","r") as f , open("output.csv","w") as out:
-    out.write("Company name , PE Ratio , PB Ratio\n")
-    next(f)
-    for line in f:
-        token = line.split(',')
-        stocks = token[0]
-        price = float(token[1])
-        eps = float(token[2])
-        book = float(token[3])
-        pe = round(price/eps,2)
-        pb = round(price/book,2)
 
-        out.write(f"{stocks} , {pe} , {pb} \n")
 
 
     
