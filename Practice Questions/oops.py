55b64eeb7ade7e4905934d538323f53b3dfb9f2e
Sahil Saeid <sahilsaeidkhan610@gmail.com>
2026-02-17 11:57:28 +0530

    oops

--- Practice Questions/oops.py
+++ Practice Questions/oops.py
@@ -113,4 +113,24 @@ acc1.credit(24000)
 
 
 
+# Define a Circle class to create a circle with radius r using the constructor.
+# Define an Area() method of the class which calculates the area of the circle.
+# Define a Perimeter() method of the class which allows you to calculate the perimeter of the circle
+
+class Circle:
+
+    def __init__(self,radius):
+        self.r = radius
+    
+    def Area(self):
+        area = 3.14 * self.r * self.r
+        print(area)
+
+    def Perimeter(self):
+        perimeter = round(2 * 3.14 * self.r,2) 
+        print(perimeter)
+
+c1 = Circle(5)
+c1.Area()
+c1.Perimeter()
 
---
+++ Practice Questions/practice.py
@@ -0,0 +1,5 @@
+# Qs. Define a Employee class with attributes role, department & salary. This class also a
+# showDetails( ) method.
+# Create an Engineer class that inherits properties from Employee & has additional
+# attributes : name & age.
+
